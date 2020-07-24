cec60d0584c7237dd921c3d973e3925ac4189fb3
Baruch Phillips <baruchphillips@gmail.com>
2020-07-24 17:05:29 -0400

    created api

--- server.js
+++ server.js
@@ -1,14 +1,15 @@
 const express = require('express')
 const mongoose = require('mongoose')
 const bodyParser = require('body-parser')
+// const router = express.Router()
 require('dotenv/config')
-const cors = require('cors')
+// const cors = require('cors')
 const app = express()
-
+const router = express.Router()
 // BodyParser Middleware
 app.use(bodyParser.json())
 // Allowing client-side to connect
-app.use(cors())
+// app.use(cors())
 
 // Connect to MongoDB
 const uri = process.env.MONGO_URI;
@@ -18,8 +19,25 @@ mongoose.connect(uri, {useNewUrlParser: true,useUnifiedTopology: true})
     .catch(() => console.log("DIDN'T CONNECT"))
 
 // Use Routes
-app.use('/test', items)
+app.use('/',router)
+
+const thirdLetter = (str) => {
+    let newStr = ""
+    for(let i=1;i<str.length;i++){
+        if(i%3 === 0){
+            newStr+=str[i-1]
+        }
+    }
+    return newStr
+}
+
+app.post('/test', (req, resp) => {
+    const str = req.body.string_to_cut
+    resp.json(thirdLetter(str))
+})
 
 const port = process.env.PORT || 5000;
 
 app.listen(port, () => console.log(`Server started on port ${port}`))
+
+module.exports = router
